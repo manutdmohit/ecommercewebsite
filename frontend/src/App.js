e72ebbc6db401afdeeaa8e9f965c24cf8134a6e3
manutdmohit <saudmohit@gmail.com>
2021-08-26 12:14:02 +0545

    bootstrapsetup,header,footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,18 @@
+import { Container } from 'react-bootstrap';
+import Header from './components/Header';
+import Footer from './components/Footer';
+
 const App = () => {
   return (
-    <div>
-      <h1>Ecommerce</h1>
-    </div>
+    <>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <h1>Welcome to Nepal Shop</h1>
+        </Container>
+      </main>
+      <Footer />
+    </>
   );
 };
 
