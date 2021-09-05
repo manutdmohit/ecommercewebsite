0f3292cf485c96737cbdb5594d09e6098bbec45c
manutdmohit <saudmohit@gmail.com>
2021-09-05 10:59:24 +0545

    generateajsonwebtoken

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,6 @@
 import asynchandler from 'express-async-handler';
 import User from '../models/userModel.js';
+import generateToken from '../utils/generateToken.js';
 
 // auth user and get token // post // /api/users/login //public
 const authUser = asynchandler(async (req, res) => {
@@ -13,7 +14,7 @@ const authUser = asynchandler(async (req, res) => {
       name: user.name,
       email: user.email,
       isAdmin: user.isAdmin,
-      token: null,
+      token: generateToken(user._id),
     });
   } else {
     res.status(401);
