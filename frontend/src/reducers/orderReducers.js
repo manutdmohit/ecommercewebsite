ba0a631b01c460a2227ad2ad6b3d5292b14a3d8d
manutdmohit <saudmohit@gmail.com>
2021-09-13 08:16:15 +0545

    order details reducer and actions

--- frontend/src/reducers/orderReducers.js
+++ frontend/src/reducers/orderReducers.js
@@ -18,3 +18,18 @@ export const orderCreateReducer = (state = {}, action) => {
       return state;
   }
 };
+
+export const orderDetailsReducer = (state = {loading: true, orderItems :[],shippingAddress:{}}, action) => {
+  switch (action.type) {
+    case ORDER_DETAILS_REQUEST:
+      return { ...state,loading: true };
+    case ORDER_DETAILS_SUCCESS:
+      return { loading: false,order: action.payload };
+
+    case ORDER_DETAILS_FAIL:
+      return { loading: false, error: action.payload };
+
+    default:
+      return state;
+  }
+};
