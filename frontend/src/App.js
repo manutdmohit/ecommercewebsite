d80748a829749cb4eccf9527bde359ff024325b0
manutdmohit <saudmohit@gmail.com>
2021-09-16 08:46:29 +0545

    admin user list

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,6 +12,7 @@ import ShippingScreen from './screens/ShippingScreen';
 import PaymentScreen from './screens/PaymentScreen';
 import PlaceOrderScreen from './screens/PlaceOrderScreen';
 import OrderScreen from './screens/OrderScreen';
+import UserListScreen from './screens/UserListScreen';
 
 const App = () => {
   return (
@@ -19,15 +20,16 @@ const App = () => {
       <Header />
       <main className="py-3">
         <Container>
-          <Route path="/order/:id" component={OrderScreen}/>
-          <Route path="/placeorder" component={PlaceOrderScreen}/>
-          <Route path="/payment" component={PaymentScreen}/>
+          <Route path="/order/:id" component={OrderScreen} />
+          <Route path="/placeorder" component={PlaceOrderScreen} />
+          <Route path="/payment" component={PaymentScreen} />
           <Route path="/shipping" component={ShippingScreen} />
           <Route path="/login" component={LoginScreen} />
           <Route path="/register" component={RegisterScreen} />
           <Route path="/profile" component={ProfileScreen} />
           <Route path="/product/:id" component={ProductScreen} />
           <Route path="/cart/:id?" component={CartScreen} />
+          <Route path="/admin/userlist" component={UserListScreen} />
           <Route exact path="/" component={HomeScreen} />
         </Container>
       </main>
